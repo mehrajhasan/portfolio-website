7260cf88dd059dc8afda896f14ec913438c14ce3
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-07-21 19:08:19 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import Header from '../components/Header'
-import Main from '../components/Main'
+import Header from './components/Header'
+import Main from './components/Main'
+import Skills from './components/Skills'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <Main/>
+      <Skills/>
     </div>
   );
 }
