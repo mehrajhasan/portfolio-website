40e4eaea43da3fd9acfbff89275b8755c79b614d
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-07-21 15:29:06 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './components/Header'
-import Main from './components/Main'
+import Header from '../components/Header'
+import Main from '../components/Main'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
