c18c54c13c47de43486cf7f20080528525a2d426
Mehraj Hasan <mehrajhasancs@gmail.com>
2024-07-22 16:23:37 -0400

    complete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,33 @@
-
+import Container from 'react-bootstrap/Container';
+import proj1 from '../assets/images/example.png';
+import wip from '../assets/images/working.gif';
 
 export const Projects = () => {
 
+    return (
+        <section className="project" id="projects">
+            <Container>
+                <div className="projects-box">
+                    <h2>Projects</h2>
+                    <p>Listed below are the projects I have created</p>
+                
+                    <div className="projects">
+                        <div className="item">
+                            <a href='https://github.com/mehrajhasan/Rock_Paper_Scissors'><img src={proj1} alt='img'/></a>
+                        </div>
+
+                        <div className="item">
+                            <img src={wip} alt='img'/>
+                        </div>
+
+                        <div className="item">
+                            <img src={wip} alt='img'/>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </section>
+    ) 
 }
 
 export default Projects;
