8c6f0b53b4c67c8c4edbf08b8f58d25f17de89f7
Mehraj Hasan <mehrajhasancs@gmail.com>
2024-07-28 22:56:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import Header from './components/Header'
-import Main from './components/Main'
-import Skills from './components/Skills'
+import Header from './components/Header';
+import Main from './components/Main';
+import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Connect from './components/Connect';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,8 +12,10 @@ function App() {
     <div className="App">
       <Header/>
       <Main/>
+      <AboutMe/>
       <Skills/>
       <Projects/>
+      <Connect/>
     </div>
   );
 }
