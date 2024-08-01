56c202667f43bed4355e59effebd5ea80d4b92b2
Mehraj Hasan <mehrajhasancs@gmail.com>
2024-08-01 14:58:04 -0400

    slide in

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,37 @@
-import Container from 'react-bootstrap/Container';
+import { Container } from 'react-bootstrap';
+import { useInView } from 'react-intersection-observer';
 import proj1 from '../assets/images/example.png';
 import wip from '../assets/images/working.gif';
 
 export const Projects = () => {
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000},
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024},
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464},
+            items: 5
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0},
+            items: 1
+        }
+    };
+
+    const { ref: project, inView: projectIsVisible } = useInView({
+        triggerOnce: true, 
+        threshold: 0.1 
+    });
 
     return (
         <section className="project" id="projects">
             <Container>
-                <div className="projects-box">
+                <div ref={project} className={`projects-box ${projectIsVisible ? 'in-view' : ''}`}>
                     <h2>Projects</h2>
                     <p>Listed below are some of the projects I have created</p>
                 
