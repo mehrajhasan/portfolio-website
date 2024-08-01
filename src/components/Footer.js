6324d084ef73578ad8b07c0368ad8e5aa2f365ac
Mehraj Hasan <mehrajhasancs@gmail.com>
2024-08-01 14:57:50 -0400

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import { Container } from 'react-bootstrap'
+export const Footer = () => {
+
+    return (
+            <div className="footer">
+                <div className="create">
+                    <h2>
+                    This site was created with React and CSS
+                    </h2>
+                </div>
+                <div className="by">
+                    <h2>
+                        Property of Mehraj Hasan
+                    </h2>
+                </div>
+
+                <div className="rights">
+                    <p>
+                        ALL RIGHTS RESERVED
+                    </p>
+                </div>
+            </div>
+    );
+}
+
+export default Footer;
